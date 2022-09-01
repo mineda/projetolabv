206a0c8b562d296b7be46ed78ea436df7b439c48
larijanine <larissajjanine@hotmail.com>
2022-09-01 23:12:58 +0000

    idcrontoller

--- src/main/java/br/gov/sp/fatec/projetolab5/controller/UsuarioController.java
+++ src/main/java/br/gov/sp/fatec/projetolab5/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class UsuarioController {
     @GetMapping
     public List<Usuario> todoUsuario() {
         return segurancaService.todosUsuarios();
-    
     }
+
+    @GetMapping(value = "/{id}")
+    public Usuario buscarPeloId(@PathVariable("id") Long id) {
+        return segurancaService.buscarUsuarioPorId(id);
+    }
+
 }
