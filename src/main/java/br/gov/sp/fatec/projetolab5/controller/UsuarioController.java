cab61261bb148379d908d0ab7ca295598c164287
larijanine <larissajjanine@hotmail.com>
2022-09-01 23:05:35 +0000

    Controllers

---
+++ src/main/java/br/gov/sp/fatec/projetolab5/controller/UsuarioController.java
@@ -0,0 +1,27 @@
+package br.gov.sp.fatec.projetolab5.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.gov.sp.fatec.projetolab5.entity.Usuario;
+import br.gov.sp.fatec.projetolab5.service.SegurancaService;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value="/usuario")
+public class UsuarioController {
+
+    @Autowired
+    private SegurancaService segurancaService;
+
+    @GetMapping
+    public List<Usuario> todoUsuario() {
+        return segurancaService.todosUsuarios();
+    
+    }
+}
