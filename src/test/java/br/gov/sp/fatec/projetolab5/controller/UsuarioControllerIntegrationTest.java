31512dbbbae141df4519c83225de8816028b5cab
Emanuel Mineda Carneiro <mineda@gmail.com>
2023-03-16 20:35:48 +0000

    Banco de dados para testes

--- src/test/java/br/gov/sp/fatec/projetolab5/controller/UsuarioControllerIntegrationTest.java
+++ src/test/java/br/gov/sp/fatec/projetolab5/controller/UsuarioControllerIntegrationTest.java
@@ -5,19 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
-@Rollback
 public class UsuarioControllerIntegrationTest {
     
     @Autowired
--- src/test/java/br/gov/sp/fatec/projetolab5/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/gov/sp/fatec/projetolab5/repository/UsuarioRepositoryTest.java
@@ -5,14 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 
 import br.gov.sp.fatec.projetolab5.entity.Usuario;
 
 @SpringBootTest
-@Transactional
-@Rollback
 public class UsuarioRepositoryTest {
 
     @Autowired
